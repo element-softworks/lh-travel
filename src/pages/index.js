267945822a631dd667a381c375178c1274d00cfd
Ryan <ryanimpey@live.co.uk>
2019-11-16 15:12:08 +0000

    Added splash styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,8 @@ import SEO from "../components/seo";
 import "../styles/pages/_index.scss";
 import "semantic-ui-css/semantic.min.css";
 
+import { Search } from "semantic-ui-react";
+
 const IndexPage = () => (
     <React.Fragment>
         <SEO title="Flight Search" />
@@ -49,14 +51,47 @@ const BackgroundSection = ({ className }) => (
                     className={className}
                     fluid={imageData}
                     backgroundColor={`#040e18`}
+                    style={{ backgroundPosition: "top center" }}
                 >
                     <section className="splash-container">
-                        <h1 className="splash-title">Hello gatsby-background-image</h1>
+                        <h1 className="splash-title">
+                            Jeff's big green flight machine
+                        </h1>
+                        <div className="splash-search">
+                            <Search placeholder="Departure" />
+                            <Search placeholder="Arrival" />
+                        </div>
                     </section>
                 </BackgroundImage>
             );
         }}
     />
 );
+
+const countryOptions = [
+    { key: "af", value: "af", flag: "af", text: "Afghanistan" },
+    { key: "ax", value: "ax", flag: "ax", text: "Aland Islands" },
+    { key: "al", value: "al", flag: "al", text: "Albania" },
+    { key: "dz", value: "dz", flag: "dz", text: "Algeria" },
+    { key: "as", value: "as", flag: "as", text: "American Samoa" },
+    { key: "ad", value: "ad", flag: "ad", text: "Andorra" },
+    { key: "ao", value: "ao", flag: "ao", text: "Angola" },
+    { key: "ai", value: "ai", flag: "ai", text: "Anguilla" },
+    { key: "ag", value: "ag", flag: "ag", text: "Antigua" },
+    { key: "ar", value: "ar", flag: "ar", text: "Argentina" },
+    { key: "am", value: "am", flag: "am", text: "Armenia" },
+    { key: "aw", value: "aw", flag: "aw", text: "Aruba" },
+    { key: "au", value: "au", flag: "au", text: "Australia" },
+    { key: "at", value: "at", flag: "at", text: "Austria" },
+    { key: "az", value: "az", flag: "az", text: "Azerbaijan" },
+    { key: "bs", value: "bs", flag: "bs", text: "Bahamas" },
+    { key: "bh", value: "bh", flag: "bh", text: "Bahrain" },
+    { key: "bd", value: "bd", flag: "bd", text: "Bangladesh" },
+    { key: "bb", value: "bb", flag: "bb", text: "Barbados" },
+    { key: "by", value: "by", flag: "by", text: "Belarus" },
+    { key: "be", value: "be", flag: "be", text: "Belgium" },
+    { key: "bz", value: "bz", flag: "bz", text: "Belize" },
+    { key: "bj", value: "bj", flag: "bj", text: "Benin" },
+];
 
 export default IndexPage;
