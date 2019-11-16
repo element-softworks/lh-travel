901ac7e2af75c5168ed0fa55f36a2b61c619b5da
Ryan <ryanimpey@live.co.uk>
2019-11-16 14:19:51 +0000

    Home page background styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, StaticQuery } from "gatsby";
+
+import BackgroundImage from "gatsby-background-image";
 
 import Layout from "../components/layout";
 import Image from "../components/image";
@@ -10,16 +12,51 @@ import "../styles/pages/_index.scss";
 import "semantic-ui-css/semantic.min.css";
 
 const IndexPage = () => (
-    <Layout>
+    <React.Fragment>
         <SEO title="Flight Search" />
-        <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <div className="test-div">
-            <Image />
-        </div>
-        <Link to="/page-2/">Go to page 2</Link>
-    </Layout>
+        <Layout>
+            <BackgroundSection />
+            <h1>Hi people</h1>
+            <p>Welcome to your new Gatsby site.</p>
+            <p>Now go build something great.</p>
+            <div className="test-div">
+                <Image />
+            </div>
+            <Link to="/page-2/">Go to page 2</Link>
+        </Layout>
+    </React.Fragment>
+);
+
+const BackgroundSection = ({ className }) => (
+    <StaticQuery
+        query={graphql`
+            query {
+                desktop: file(relativePath: { eq: "green_splash.jpg" }) {
+                    childImageSharp {
+                        fluid(quality: 90, maxWidth: 1920) {
+                            ...GatsbyImageSharpFluid_withWebp
+                        }
+                    }
+                }
+            }
+        `}
+        render={data => {
+            // Set ImageData.
+            const imageData = data.desktop.childImageSharp.fluid;
+            return (
+                <BackgroundImage
+                    Tag="section"
+                    className={className}
+                    fluid={imageData}
+                    backgroundColor={`#040e18`}
+                >
+                    <section className="splash-container">
+                        <h1>Hello gatsby-background-image</h1>
+                    </section>
+                </BackgroundImage>
+            );
+        }}
+    />
 );
 
 export default IndexPage;
