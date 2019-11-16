0b2e6049ab59b9790449d5df0b171916ae596669
Ryan <ryanimpey@live.co.uk>
2019-11-16 13:58:19 +0000

    Added Jeff logo to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,44 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React from "react";
+import PropTypes from "prop-types";
+import Img from "gatsby-image";
+import { Link, useStaticQuery } from "gatsby";
+
+import "../styles/components/_header.scss";
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+    <header className="header">
+        <div className="header-container">
+            <Link to="/">
+                <Logo className="header-logo" />
+            </Link>
+        </div>
+    </header>
+);
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+    siteTitle: PropTypes.string,
+};
 
 Header.defaultProps = {
-  siteTitle: ``,
-}
+    siteTitle: ``,
+};
 
-export default Header
+const Logo = ({ ...props }) => {
+    const data = useStaticQuery(graphql`
+        query {
+            placeholderImage: file(relativePath: { eq: "logo.png" }) {
+                childImageSharp {
+                    fluid(maxWidth: 300) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+        }
+    `);
+
+    return (
+        <Img {...props} fluid={data.placeholderImage.childImageSharp.fluid} />
+    );
+};
+
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,25 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
 
 // Style Import
-import "../styles/pages/_index.scss"
-import "semantic-ui-css/semantic.min.css"
+import "../styles/pages/_index.scss";
+import "semantic-ui-css/semantic.min.css";
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Flight Search" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div className="test-div">
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+    <Layout>
+        <SEO title="Flight Search" />
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <div className="test-div">
+            <Image />
+        </div>
+        <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+);
 
-export default IndexPage
+export default IndexPage;
